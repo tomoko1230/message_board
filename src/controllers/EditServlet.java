4490918f8faa19a41e28c58ecc7c8cc5d9c31fde
tomoko.maegawa <tomoko.maegawa@g.softbank.co.jp>
2022-11-26 17:49:58 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -41,6 +41,10 @@ public class EditServlet extends HttpServlet {
         // メッセージ情報とセッションIDをリクエストスコープに登録
         request.setAttribute("message", m);
         request.setAttribute("_token", request.getSession().getId());
+        
+        if(m != null) {
+            request.getSession().setAttribute("message_id", m.getId());
+        }
 
         // メッセージIDをセッションスコープに登録
         request.getSession().setAttribute("message_id", m.getId());
